0e210c83576336dc071aff0266a1db97b5cc78f0
Will <wsg.online@gmail.com>
2022-06-26 00:48:14 -0300

    docs: added new photos to gallery

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -4,10 +4,28 @@ import photo3 from "../Images/gallery/LucasCalzoni2.jpg";
 import photo4 from "../Images/gallery/ReiDoShow2018.jpg";
 import photo5 from "../Images/gallery/ReiDoShow20182.jpg";
 import photo6 from "../Images/gallery/Viva2019.jpg";
+import photo7 from "../Images/gallery/LucasCalzoni3.jpg";
+import photo8 from "../Images/gallery/LucasCalzoni4.jpg";
+import photo9 from "../Images/gallery/LucasCalzoni5.jpg";
+import photo10 from "../Images/gallery/LucasCalzoni6.jpg";
+import photo11 from "../Images/gallery/LucasCalzoni7.jpg";
+
 import Photo from "./Photo";
 
 export default function Gallery() {
-  const photos = [photo1, photo2, photo3, photo4, photo5, photo6];
+  const photos = [
+    photo1,
+    photo2,
+    photo3,
+    photo4,
+    photo5,
+    photo6,
+    photo7,
+    photo8,
+    photo9,
+    photo10,
+    photo11,
+  ];
 
   return (
     <div id="gallery">
